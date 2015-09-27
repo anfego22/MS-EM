4b934a5ab20e0a082558372009ff2a97b262647a
Andres <anfego22@gmail.com>
2015-09-26 19:41:10 -0500

    Adjust iteration boundary when calculating XiS

--- filterS.cpp
+++ filterS.cpp
@@ -1,12 +1,13 @@
 #include "EM_Classes.h"
 
 void Xi::filterProb(){
-	Xitt.setZero(model.Nm, errors.T);
-	Xit1t.setZero(model.Nm, errors.T+1);
+	Xitt.setZero(Nm, errors.T);
+	Xit1t.setZero(Nm, errors.T+1);
 	Xit1t.col(0) = param.rhoF.rho;
-
+	llikel.setZero(errors.T);
+	
 	for (int t = 0; t< errors.T; t++){
-		llikel(t) = Xit1t.col(t).dot(errors.eta.col(t));
+		llikel[t] = Xit1t.col(t).dot(errors.eta.col(t));
 		Xitt.col(t) = Xit1t.col(t).cwiseProduct(errors.eta.col(t));
 		Xitt.col(t) /= llikel(t);
 		Xit1t.col(t+1) = param.rhoF.F*Xitt.col(t);
@@ -22,9 +23,9 @@ void Xi::filterProb(){
 }
 
 void Xi::smoothProb(){
-	XiS.setZero(model.Nm, errors.T+1);
+	XiS.setZero(Nm, errors.T);
 	XiS.col(errors.T - 1) = Xitt.col(errors.T - 1);
-	for (int t = errors.T - 2; t!= 0; t--){
+	for (int t = errors.T - 2; t != -1; t--){
 		XiS.col(t) = Xitt.col(t).cwiseProduct(
 			param.rhoF.F.transpose()*
 			(XiS.col(t+1).cwiseQuotient(Xit1t.col(t+1)))
@@ -35,6 +36,7 @@ void Xi::smoothProb(){
 
 Xi::Xi(const Errors &err):errors(err), param(err.param),
 						  data(err.data), model(err.model){
+	Nm = errors.eta.rows();
 	filterProb();
 	smoothProb();
 }
